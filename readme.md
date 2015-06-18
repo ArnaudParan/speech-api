41e646402a13237fa7f2c8ff5c5870129a5d8a97
Thibault Pouget <thibault.pouget@cirruseo.com>
2015-06-18 21:12:51 +0200

    fixes

---
+++ readme.md
@@ -0,0 +1,6 @@
+Quick start
+
+- fork sur bitbucket
+- attention aux submodules qui se checkout pas bien !
+- pycharm source folders (dependencies, cirruseo_custom_framework)
+... to be continued
--- settings.py
+++ settings.py
@@ -2,21 +2,29 @@
 # -*- coding: utf-8 -*-
 
 from google.appengine.api import app_identity, modules
-from cirruslib.handlers.auth.callback import OAuth2FmkCallback
+
+# SCOPES
+ADMIN_SDK_USERS_READ_ONLY = 'https://www.googleapis.com/auth/admin.directory.user.readonly'
+ADMIN_SDK_GROUPS_READ_ONLY = 'https://www.googleapis.com/auth/admin.directory.group.readonly'
+ADMIN_SDK_MEMBERS_READ_ONLY = 'https://www.googleapis.com/auth/admin.directory.group.member.readonly'
+GOOGLE_PLUS_PROFILE = 'https://www.googleapis.com/auth/plus.login'
+GOOGLE_PLUS_EMAILS = 'https://www.googleapis.com/auth/plus.profile.emails.read'
 
 # App auth settings
 # TODO Generate a Web Application Client ID for your project and change the following values
 CLIENT_ID = 'MyClientId'
 CLIENT_SECRET = 'MyClientSecret'
-OAUTH2_CALLBACK = OAuth2FmkCallback.URL
+OAUTH2_CALLBACK = '/oauth2callback'
 
 # API auth settings
 # TODO Generate a Service Account Client ID for your project and change the following values
-SERVICE_ACCOUNT_CLIENT_ID = 'MyServiceAccountClientId'
 SERVICE_ACCOUNT = 'MyServiceAccount'
 SCOPES = [
-    'MyScope1',
-    'MyScope2'
+    ADMIN_SDK_USERS_READ_ONLY,
+    ADMIN_SDK_GROUPS_READ_ONLY,
+    ADMIN_SDK_MEMBERS_READ_ONLY,
+    GOOGLE_PLUS_PROFILE,
+    GOOGLE_PLUS_EMAILS
 ]
 PRIVATE_KEY_FILENAME = 'MyPrivateKeyFilename.pem'
 
@@ -30,7 +38,6 @@ PRIVATE_KEY_PATH = 'key/%s' % PRIVATE_KEY_FILENAME
 # !!! You should not have to modify this. Actually just don't !!!
 GOOGLE_API_SETTINGS = {
     'pem_private_key_path': PRIVATE_KEY_PATH,
-    'client_id': CLIENT_ID,
     'service_account': SERVICE_ACCOUNT,
     'user_agent': USER_AGENT,
     'scopes': SCOPES
@@ -39,5 +46,3 @@ GOOGLE_API_SETTINGS = {
 # TODO Change these customer specific settings
 DOMAIN = ''
 ADMIN_EMAIL = ''
-
-
